68152c7dc382eea936d2af5497b960b6e86062de
Mateus LOPES <101558333+MateusLopes16@users.noreply.github.com>
2026-01-12 15:43:04 +0100

    Install required packages and load datasets
    Added package installations and data loading.

--- script.r
+++ script.r
@@ -1,1 +1,18 @@
 # script file
+install.packages(c("cluster", "rpart", "C50", "tree", "randomForest", "kknn", "ROCR"))
+
+library(cluster)
+library(rpart)
+library(C50)
+library(tree)
+library(randomForest)
+library(kknn)
+library(ROCR)
+
+# Chargement des données
+data <- read.csv("Data_Projet.csv", header = TRUE, sep = ",", dec = ".",
+                 stringsAsFactors = TRUE)
+data_new <- read.csv("Data_Projet_New.csv", header = TRUE, sep = ",", dec = ".",
+                     stringsAsFactors = TRUE)
+
+
